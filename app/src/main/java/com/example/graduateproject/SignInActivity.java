fe90293a6789becfc222ca836c98ae1a93143f9d
Aya Maher <aya.nassan2000@gmail.com>
2022-04-26 19:07:52 +0300

    graduate_app

---
+++ app/src/main/java/com/example/graduateproject/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.example.graduateproject;
+
+import static com.example.graduateproject.R.layout.activity_sign_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_sign_in);
+    }
+}
